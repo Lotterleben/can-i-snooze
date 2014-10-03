a7f32fcf6c4a2a0b1d4c53e7f89ff1d89dd4c59a
Lotte Steenbrink <lotte@zombietetris.de>
2014-10-03 12:54:09 +0200

    functionification; automatically figure out right date for req-str

--- canisnooze.py
+++ canisnooze.py
@@ -1,63 +1,55 @@
 #!/usr/bin/env python
 import requests
+import datetime
 import re
 from bs4 import BeautifulSoup
 
-# TODO: add correct day (-> aus woche ableiten?)
-ICE_name = "ICE  209"
-#ICE_name = "ICE"
-req_str = "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?&rt=1&input=Berlin+Hbf&time=12:00&date=03.10.14&productsFilter=1000000000&start=1&boardType=arr&REQTrain_name=209"
+def next_tuesday(day):
+    days_ahead = 1 - day.weekday()
 
-print "fetching Verbindungsinformationen..."
-req = requests.get(req_str)
-soup = BeautifulSoup(req.text)
+    if days_ahead < 0:
+        days_ahead += 7
 
-rows = soup.find_all('tr', {'id':re.compile('journeyRow_.*')})
+    return day + datetime.timedelta(days_ahead)
 
-for row in rows:
-    train = row.find_all('a', text=re.compile(".*ICE.*"))[0].text
-    info = row.find_all('td', {'class':'ris'})[0]
+def get_delay_info(day, train_name):
+    #train_name = "ICE"
+    day = "03.10.14"
+    req_str = "http://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?&rt=1&input=Berlin+Hbf&time=12:00&date=%s&productsFilter=1000000000&start=1&boardType=arr&REQTrain_name=209" % (day)
 
-    if (ICE_name in train):
-        delay_info = info.find_all('span', text=re.compile(".*ca\. \+*"))
-        delay = 0
+    print "Fetching Verbindungsinformationen..."
+    req = requests.get(req_str)
+    soup = BeautifulSoup(req.text)
 
-        # if there is info about a delay, there is a delay (duh)
-        if (delay_info):
+    rows = soup.find_all('tr', {'id':re.compile('journeyRow_.*')})
 
-            # for some reason there may be more than 1 in the list
-            for i in delay_info:
-                time = re.search(".*ca\. \+(.*)", i.text).groups()[0]
-                if (time > delay):
-                    delay = time
+    for row in rows:
+        train = row.find_all('a', text=re.compile(".*ICE.*"))[0].text
+        info = row.find_all('td', {'class':'ris'})[0]
 
-            print "train: ", ICE_name
-            print "delay: ", delay
+        if (train_name in train):
+            delay_info = info.find_all('span', text=re.compile(".*ca\. \+*"))
+            delay = 0
 
-        # TODO: return instead of print
-        print {"train", ICE_name, "delay", delay}
+            # if there is info about a delay, there is a delay (duh)
+            if (delay_info):
 
+                # for some reason there may be more than 1 in the list
+                for i in delay_info:
+                    time = re.search(".*ca\. \+(.*)", i.text).groups()[0]
+                    if (time > delay):
+                        delay = time
 
+                print "train: ", train_name
+                print "delay: ", delay
 
+            return {"train", train_name, "delay", delay}
 
+if __name__ == "__main__":
+    today = datetime.date.today()
+    train_name = "ICE  209"
+    next_tuesday = next_tuesday(today).strftime("%d.%m.%y")
+    print("Getting delay info for %s on tuesday, %s ..." % (train_name, next_tuesday))
+    #print get_delay_info(next_tuesday, train_name)
+    print get_delay_info(today.strftime("%d.%m.%y"), train_name)
 
-
-
-
-
-
-
-'''
-req_str = "http://reiseauskunft.bahn.de/bin/query.exe/dn?revia=yes&existOptimizePrice=1&country=DEU&dbkanal_007=L01_S01_D001_KIN0001_qf-bahn_LZ003&ignoreTypeCheck=yes&S=BERLIN&REQ0JourneyStopsSID=&REQ0JourneyStopsS0A=7&Z=HAMBURG&REQ0JourneyStopsZID=&REQ0JourneyStopsZ0A=7&trip-type=single&date=Di%2C+07.10.14&time=05%3A45&timesel=depart&returnTimesel=depart&optimize=0&travelProfile=-1&adult-number=1&children-number=0&infant-number=0&tariffTravellerType.1=E&tariffTravellerReductionClass.1=0&tariffTravellerAge.1=&qf-trav-bday-1=&tariffTravellerReductionClass.2=0&tariffTravellerReductionClass.3=0&tariffTravellerReductionClass.4=0&tariffTravellerReductionClass.5=0&tariffClass=2&start=1&qf.bahn.button.suchen="
-
-print "fetching Verbindungsinformationen..."
-req = requests.get(req_str)
-soup = BeautifulSoup(req.text)
-
-times = soup.find_all('td', {'class':"time"})[1:-1]
-print times, "\n xxxxxxxxxxxx"
-print type(times)
-
-time = soup.find('td', {'class':"time"}, text=re.compile(".*45.*"))
-print time
-'''
