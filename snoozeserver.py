eff76db2f09b835dcfdc4341e8432fd141fd3921
Lotte Steenbrink <lotte@zombietetris.de>
2014-10-06 22:07:25 +0200

    + basic snoozeserver

---
+++ snoozeserver.py
@@ -0,0 +1,26 @@
+import datetime
+from bottle import route, run
+from canisnooze import get_delay_info, next_tuesday
+
+@route('/')
+def display_delay():
+    today = datetime.date.today()
+    train_name = "ICE 1518"
+
+    tuesday = next_tuesday(today).strftime("%d.%m.%y")
+    info = get_delay_info(tuesday, train_name)
+    delay = info["delay"]
+
+    if (delay == -2):
+        answer = "I have no idea, something went terribly wrong. Sorry."
+    elif (delay == -1):
+        answer = "I don't know yet, please come back later."
+    elif (delay == 0):
+        answer = "No."
+    else:
+        # TODO: make delay time stand out (wo mach ich das am besten?)
+        answer = "Yes! %i more minutes." % delay
+
+    return answer
+
+run(host='localhost', port=8080, debug=True)
